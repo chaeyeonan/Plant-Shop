cdfc4197749b6eaa49e8dde5b55e019761551d34
chaeyeonan <dkscodus1204@ewhain.net>
2022-11-29 12:49:49 +0900

    feat: landing & about section

--- soap-shop/js/custom.js
+++ soap-shop/js/custom.js
@@ -26,6 +26,17 @@ const toggleMobileBtn = (e) => {
 
 mobileBtn.addEventListener("click", toggleMobileBtn);
 
+// header change Effect
+const header = document.querySelector("#header");
+const stickyHeader = () => {
+  const scrY = window.scrollY;
+
+  if (scrY > 0) header.classList.add("active");
+  else header.classList.remove("active");
+};
+
+window.addEventListener("scroll", stickyHeader);
+
 // Scrollreveal Effect
 const sr = ScrollReveal({ reset: false });
 sr.reveal(".wrapper", { durationi: 1000 });
