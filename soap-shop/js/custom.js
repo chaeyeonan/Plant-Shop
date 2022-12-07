fa458b8d467b72e9c072d02d9893de97fe7ecf8b
chaeyeonan <dkscodus1204@ewhain.net>
2022-12-07 11:24:47 +0900

    🎨 feat:  header.js 분리

--- soap-shop/js/custom.js
+++ soap-shop/js/custom.js
@@ -1,31 +1,3 @@
-// Elements Clone For Mobile
-const mobileMenus = document.querySelector(".mobile-menus");
-const navs = document.querySelector(".nav-lists").cloneNode(true);
-const info = document.querySelector(".info").cloneNode(true);
-
-mobileMenus.appendChild(navs);
-mobileMenus.appendChild(info);
-
-// Mobile Menu Toggle
-const mobileBtn = document.querySelector(".mobile-btn");
-
-const toggleMobileBtn = (e) => {
-  const target = e.currentTarget;
-  const menuHeight = mobileMenus.scrollHeight;
-
-  target.classList.toggle("active");
-
-  if (target.classList.contains("active")) {
-    target.classList.remove("not-active");
-    mobileMenus.style.height = `${menuHeight}px`;
-  } else {
-    target.classList.add("not-active");
-    mobileMenus.style.height = 0;
-  }
-};
-
-mobileBtn.addEventListener("click", toggleMobileBtn);
-
 // header change Effect
 const header = document.querySelector("#header");
 const stickyHeader = () => {
